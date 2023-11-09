ebe5546caa9a781caab5b807495778f273656ba8
Abdul <Abdul@example.com>
2023-11-09 11:43:40 +0530

    n

---
+++ src/main/java/org/example/Multiplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Multiplication {
+    static  int multiply(ArrayList<Integer>array){
+        int prod =0;
+        for(int i :array)
+            prod *=1;
+        return prod;
+    }
+}
